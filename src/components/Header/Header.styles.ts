f19f141c03b9d483ce049f3a74fcb61d55f05878
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-07-20 18:45:05 -0300

    feat: add Header component

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,3 +1,59 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const HeaderWrapper = styled.header``;
+interface HeaderProps {
+  headerSticky: boolean;
+}
+
+export const Container = styled.header<HeaderProps>`
+  ${({ theme, headerSticky }) => css`
+    height: 5.75rem;
+    border-bottom: 1px solid ${theme.colors.opacityBlack};
+    position: sticky;
+    top: 0;
+    z-index: 9999;
+    background: ${headerSticky ? theme.colors.white : "transparent"};
+
+    transition: background-color 0.3s ease-in-out;
+    transform-style: flat;
+
+    div {
+      max-width: 1280px;
+      height: 5.75rem;
+      margin: 0 auto;
+      padding: 0 2rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  `}
+`;
+
+export const NavContainer = styled.nav<HeaderProps>`
+  ${({ theme, headerSticky }) => css`
+    display: flex;
+    align-items: center;
+
+    ul {
+      display: flex;
+      color: ${headerSticky ? theme.colors.white : theme.colors.darkBlue};
+      font-weight: ${theme.fonts.weight.bold};
+
+      li {
+        + li {
+          margin-left: 4rem;
+        }
+        &:hover {
+          font-weight: ${theme.fonts.weight.bold};
+        }
+      }
+    }
+
+    button {
+      margin-left: 5rem;
+
+      box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
+        rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+    }
+  `}
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,59 @@
-import { HeaderWrapper } from "./Header.styles";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useEffect, useState } from "react";
 
-export function Header(): JSX.Element {
+import Image from "next/image";
+import Link from "next/link";
+
+import * as S from "./Header.styles";
+
+export function Header() {
+  const [headerSticky, setHeaderSticky] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY >= 92) {
+      setHeaderSticky(true);
+    } else {
+      setHeaderSticky(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <HeaderWrapper>
-      <h1>Hello World</h1>
-    </HeaderWrapper>
+    <S.Container headerSticky={headerSticky}>
+      <div>
+        <Image
+          src={
+            headerSticky ? "/images/logo-footer.svg" : "/images/logo-header.svg"
+          }
+          alt="Amazing Taiwan"
+          width={146}
+          height={56}
+          style={{ cursor: "pointer" }}
+        />
+        <S.NavContainer headerSticky={headerSticky}>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Explore</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+          <button type="button">Sign In</button>
+        </S.NavContainer>
+      </div>
+    </S.Container>
   );
 }
