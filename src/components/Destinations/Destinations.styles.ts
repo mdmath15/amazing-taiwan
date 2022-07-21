5768fe6055cbf280802125302b40f5d8c1629842
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-07-21 18:28:11 -0300

    refactor: changes the use of the theme in styles

--- src/components/Destinations/Destinations.styles.ts
+++ src/components/Destinations/Destinations.styles.ts
@@ -1,18 +1,20 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Container = styled.section`
-  background: ${({ theme }) => theme.colors.darkBlue};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-  padding-bottom: 1rem;
+  ${({ theme }) => css`
+    background: ${theme.colors.darkBlue};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    padding-bottom: 1rem;
 
-  h1 {
-    color: ${({ theme }) => theme.colors.white};
-    font-size: 2.25rem;
-    text-align: center;
-    padding: 2rem 0 1rem;
-    transition: shadow 0.3s ease-in-out;
-  }
+    h1 {
+      color: ${theme.colors.white};
+      font-size: 2.25rem;
+      text-align: center;
+      padding: 2rem 0 1rem;
+      transition: shadow 0.3s ease-in-out;
+    }
+  `}
 `;
