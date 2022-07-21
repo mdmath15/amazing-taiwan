ab9ac1a02d66614c2ee173d0a60947f54e2cd627
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-07-21 14:49:10 -0300

    fix: fix storybook can't resolve alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,21 @@
+const path = require("path"); // 👈 import path
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src/"),
+    };
+    config.resolve.plugins.push(new TsconfigPathsPlugin())
+    return config;
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,30 @@
+import * as NextImage from 'next/image'
+import  GlobalStyle from '@styles/globals'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../src/styles/theme'
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  )
+]
+
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />
+})
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+      date: /Date$/
+    }
+  }
 }
