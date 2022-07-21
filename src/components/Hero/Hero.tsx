4b68b92aa4306923acb1a5873e9afa64dc9086a5
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-07-21 15:29:56 -0300

    feat: add Hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import { Button } from "../Button";
+import * as S from "./Hero.styles";
+
+export function Hero() {
+  return (
+    <S.Container>
+      <section>
+        <h1>
+          Discover your next <br /> place to travel <br /> in Taiwan
+        </h1>
+        <p>
+          Since 2020, we’ve guided more than
+          <br /> 100.000 people to enjoy their best
+          <br /> <strong>Taiwan Experience</strong>
+        </p>
+        <Button type="button" background="orange" color="white">
+          Let&apos;s Explore
+        </Button>
+      </section>
+    </S.Container>
+  );
+}
