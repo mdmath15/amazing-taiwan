620336f0b9b894606668ffccbe9343970c1b8018
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-07-22 12:27:43 -0300

    refactor: add Wave component in Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,22 +1,26 @@
 import { Button } from "../Button";
+import { Wave } from "../Wave";
 import * as S from "./Hero.styles";
 
 export function Hero() {
   return (
-    <S.Container>
-      <section>
-        <h1>
-          Discover your next <br /> place to travel <br /> in Taiwan
-        </h1>
-        <p>
-          Since 2020, we’ve guided more than
-          <br /> 100.000 people to enjoy their best
-          <br /> <strong>Taiwan Experience</strong>
-        </p>
-        <Button type="button" background="orange" color="white" withGlow>
-          Let&apos;s Explore
-        </Button>
-      </section>
-    </S.Container>
+    <>
+      <S.Container>
+        <section>
+          <h1>
+            Discover your next <br /> place to travel <br /> in Taiwan
+          </h1>
+          <p>
+            Since 2020, we’ve guided more than
+            <br /> 100.000 people to enjoy their best
+            <br /> <strong>Taiwan Experience</strong>
+          </p>
+          <Button type="button" background="orange" color="white" withGlow>
+            Let&apos;s Explore
+          </Button>
+        </section>
+      </S.Container>
+      <Wave wave="/images/wave-top.svg" width="1440" height="131" />
+    </>
   );
 }
