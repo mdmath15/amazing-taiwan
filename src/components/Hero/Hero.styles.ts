75709562efea4b8fecfca13fc3ab4a012a0c7c41
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-07-22 15:44:33 -0300

    refactor: change styles in hero and add responsiveness up to 600px

--- src/components/Hero/Hero.styles.ts
+++ src/components/Hero/Hero.styles.ts
@@ -1,35 +1,92 @@
 import styled, { css } from "styled-components";
 
-export const Container = styled.div`
+export const Content = styled.div`
   ${({ theme }) => css`
-    padding: 0 0 2rem 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
 
-    section {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 10rem 2rem;
-      background: url("/images/hero-image.svg") no-repeat right;
+    h1 {
+      color: ${theme.colors.darkBlue};
+      font-size: 3.125rem;
+      margin-top: 2rem;
 
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      gap: 1rem;
+      @media (max-width: 1440px) {
+        font-size: 2.5rem;
+      }
 
-      h1 {
-        color: ${theme.colors.darkBlue};
-        font-size: 3.125rem;
-        margin-top: 2rem;
+      @media (max-width: 980px) {
+        font-size: 2rem;
+      }
+
+      @media (max-width: 800px) {
+        font-size: 1.75rem;
       }
 
       p {
         color: ${theme.colors.darkBlue};
         font-size: 1.25rem;
         font-weight: ${theme.fonts.weight.normal};
-      }
 
-      strong {
-        font-weight: ${theme.fonts.weight.bold};
+        @media (max-width: 1240px) {
+          font-size: 1.15rem;
+        }
+
+        @media (max-width: 980px) {
+          font-size: 1rem;
+        }
       }
     }
+
+    strong {
+      font-weight: ${theme.fonts.weight.bold};
+    }
   `}
+`;
+
+export const ImageWrapper = styled.div`
+  span {
+    position: absolute;
+    right: 0;
+    max-width: 780px;
+    max-height: 632px;
+    height: 100%;
+    width: 100%;
+    background-image: url("/images/hero-image.svg");
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    @media (max-width: 1440px) {
+      position: relative;
+      background-size: contain;
+      margin-right: 2rem;
+    }
+
+    @media (max-width: 800px) {
+      margin-right: none;
+      background-position: center;
+    }
+  }
+`;
+
+export const Container = styled.section`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 8rem 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+
+  @media (max-width: 1440px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: center;
+    padding: 4rem 2rem;
+  }
+
+  @media (max-width: 800px) {
+    justify-items: normal;
+  }
 `;
