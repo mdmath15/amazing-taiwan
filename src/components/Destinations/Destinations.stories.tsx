fc10656d6d6b9c4b57e20591f1fc910e05cccccc
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-07-28 21:43:18 -0300

    chore: Add Destinations story

---
+++ src/components/Destinations/Destinations.stories.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/function-component-definition */
+/* eslint-disable prettier/prettier */
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Destinations } from "./Destinations";
+
+export default {
+  title: "Components/Destinations",
+  component: Destinations,
+  parameters: {
+    layout: "fullscreen",
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
+  },
+} as ComponentMeta<typeof Destinations>;
+
+const Template: ComponentStory<typeof Destinations> = (args) => (
+  <Destinations />
+);
+
+export const Default = Template.bind({});
